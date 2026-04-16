75a1a3ae5326cd7241e739cc33953c142330a73f
Iker Bautista <ikerdayan2006@gmail.com>
2026-04-16 00:46:31 -0600

    Corrigiendo rutas en server.js

--- back/server.js
+++ back/server.js
@@ -48,15 +48,15 @@ const tokensRecuperacion = {};
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS, // Asegúrate de que coincida con "DB_PASS" de tu captura
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: 3306, // Forza el puerto 3306 para la base de datos
-
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        rejectUnauthorized: false // Esto permite la conexión segura externa
+    },
     waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    connectionLimit: 10
 });
-
 // ==============================
 // TEST DE CONEXIÓN
 // ==============================
