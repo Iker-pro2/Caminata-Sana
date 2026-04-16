aaa6df64b4176e45b26043270ec1be8694b02367
Iker Bautista <ikerdayan2006@gmail.com>
2026-04-15 20:33:16 -0600

    Corrigiendo rutas en server.js

--- back/server.js
+++ back/server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors({
     origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning'] // <-- Agrega esto
 }));
 
 app.use(express.json());
